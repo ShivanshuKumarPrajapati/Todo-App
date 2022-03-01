f8cbdd0d031573725ba6d9ae67c6eb96f221945a
ShivanshuKumarPrajapati <shivanshu.rkt@gmail.com>
2022-03-01 16:36:53 +0530

    complete layout of project

--- frontend/src/component/savedNote/noteList.js
+++ frontend/src/component/savedNote/noteList.js
@@ -4,11 +4,18 @@ import { FaEdit, FaTrash } from "react-icons/fa";
 function noteList() {
     return (
         <div>
+            <div className="button">
+            <FaEdit className="editIcon" />
+            <FaTrash className="trashIcon" />
+            </div>
             <h1>hello</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur deleniti magni nesciunt iste tempore minima sint illum! Necessitatibus, consequatur accusamus.</p>
-            <button><FaEdit/></button>
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+            deleniti magni nesciunt iste tempore minima sint illum!
+            Necessitatibus, consequatur accusamus.
+            </p>
         </div>
-    )
+    );
 }
 
 export default noteList
